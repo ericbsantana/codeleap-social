14ef8406f602db847a66bb90bfdcca4dff0c4ef9
ericbsantana <bitencourteric@hotmail.com>
2021-08-09 18:36:39 -0300

    add clear localstorage when logout

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,5 +1,18 @@
 import { userReducer } from "./user";
 import { postReducer } from "./post";
+import { LOGOUT_USER } from "../../actions/types";
+
 import { combineReducers } from "redux";
 
-export const rootReducer = combineReducers({ userReducer, postReducer });
+const appReducer = combineReducers({ userReducer, postReducer });
+
+const rootReducer = (state, action) => {
+  if (action.type === LOGOUT_USER) {
+    state = undefined;
+    localStorage.setItem("store", JSON.stringify(state));
+  }
+
+  return appReducer(state, action);
+};
+
+export default rootReducer;
