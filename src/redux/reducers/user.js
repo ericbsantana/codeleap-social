6d43f704593f379e46e5302806bdd00142d48888
ericbsantana <bitencourteric@hotmail.com>
2021-08-09 17:52:05 -0300

    add localstorage

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,12 +1,19 @@
 import { LOGIN_USER, LOGOUT_USER } from "../../actions/types";
-const INITIAL_STATE = { username: "" };
+
+let user = "";
+if (JSON.parse(localStorage.getItem("store"))) {
+  user = JSON.parse(localStorage.getItem("store")).userReducer.username;
+  console.log(user);
+}
+
+const INITIAL_STATE = { username: user };
 
 export const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case LOGIN_USER:
-      return { ...state, username: action.payload };
+      return { username: action.payload.username };
     case LOGOUT_USER:
-      return { ...state, username: "" };
+      return { username: "" };
     default:
       return state;
   }
