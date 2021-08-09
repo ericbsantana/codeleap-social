22c3390d845a7738b695da8c5baab31cfc345e67
ericbsantana <bitencourteric@hotmail.com>
2021-08-09 11:06:57 -0300

    change index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { Provider } from "react-redux";
-
-import { store } from "./redux/store";
 import App from "./App";
+
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 ReactDOM.render(
   <React.StrictMode>
